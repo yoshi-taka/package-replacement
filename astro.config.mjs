6d1ec33a1e7eda33d6d9200d258947000b1fa3e6
yoshi-taka <3677119+yoshi-taka@users.noreply.github.com>
2026-04-08 02:25:20 +0900

    Add sitemap and robots support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
   site: "https://package-replacement.veritycost.com",
+  integrations: [sitemap()],
   server: {
     host: true,
   },
